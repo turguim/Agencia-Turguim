1856d4e5335577323d416f37db56823b4a030958
Juan Souza <juan.souza125@gmail.com>
2025-08-14 17:02:07 -0300

    site em produção versão 001

---
+++ script.js
@@ -0,0 +1,14 @@
+function ajustarContainer(){
+  const container = document.getElementById('container');
+
+  if (window.innerWidth <= 700) {
+    container.style.width = '100%'
+    container.style.marginTop = '0';
+  } else if(window.innerWidth  >700 && window.innerWidth <=1200){
+        container.style.width = '70%'
+  }else{
+        container.style.width = '40%'
+  }
+}
+window.addEventListener('load', ajustarContainer);
+window.addEventListener('resize', ajustarContainer);
